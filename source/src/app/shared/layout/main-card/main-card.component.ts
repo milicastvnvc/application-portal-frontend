32ae6f3dedc0b57a84dd506063ba567b596c33c0
Aleksandar Milanovic <62-2020@pmf.kg.ac.rs>
2024-08-25 14:20:11 +0200

    Added the full name of the candidate above the email (Application Data form)

--- source/src/app/shared/layout/main-card/main-card.component.ts
+++ source/src/app/shared/layout/main-card/main-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PersonalDetailsService } from 'src/app/personal-details/services/personal-details.service';
+import { routeCheck } from '../../helpers/route-checker';
 
 @Component({
   selector: 'app-main-card',
@@ -24,4 +27,34 @@ export class MainCardComponent {
 
   @Input()
   form_name: string | undefined = undefined;
+
+  applicationId: number | undefined;
+  fullName: string = '';
+
+
+  constructor(private route: ActivatedRoute,private personalDetailsService: PersonalDetailsService) { }
+  ngOnInit(): void {
+    this.applicationId = routeCheck(this.route);
+    // console.log(this.applicationId);
+    if (this.applicationId){
+      this.getFullName();
+    }
+    
+  }
+
+  getFullName() {
+    this.personalDetailsService.getByApplicationId(this.applicationId!).subscribe(
+      {
+        next: (result) => {
+          if (result.success) {
+            this.fullName = result.data.form?.surname + " " + result.data.form?.fornames;
+            // console.log(this.fullName);
+          }
+        },
+        error: (error) => {
+          console.error('Error retrieving personal details:', error);
+        }
+      }
+    )
+  }
 }
