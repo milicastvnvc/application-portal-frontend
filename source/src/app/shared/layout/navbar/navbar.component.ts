efeb60f3473deb8b36d12983e2b025145d1ad120
Aleksandar Milanovic <62-2020@pmf.kg.ac.rs>
2024-09-11 16:52:47 +0200

    Added Home institution name beside coordinator panel

--- source/src/app/shared/layout/navbar/navbar.component.ts
+++ source/src/app/shared/layout/navbar/navbar.component.ts
@@ -7,6 +7,8 @@ import { Collapse } from 'bootstrap';
 import { AccountService } from 'src/app/shared/services/account.service';
 import { User } from 'src/app/auth/models/user-response';
 import { erasmusLogo, uniLogo } from '../../helpers/constants';
+import { HomeInstitutionsService } from 'src/app/application/services/home-institutions.service';
+import { ApplicationService } from 'src/app/application/services/application.service';
 
 @Component({
   selector: 'app-navbar',
@@ -21,10 +23,14 @@ export class NavbarComponent {
   isCoordinator:boolean = false;
   uniLogo: string = '../../../../assets/' + uniLogo;
   erasmusLogo: string = '../../../../assets/' + erasmusLogo;
+  coordinatorInstitutionName: string | undefined;
+
 
   constructor(private authService: AuthService,
      private tokenService: TokenService,
      private accountService: AccountService,
+     private homeInstitutionsService: HomeInstitutionsService,
+     private applicationService: ApplicationService,
      private router: Router) { }
 
   ngOnInit(): void {
@@ -39,6 +45,10 @@ export class NavbarComponent {
           this.user = this.accountService.getUserData();
           this.isAdmin = this.accountService.isAdmin();
           this.isCoordinator = this.accountService.isCoordinator();
+
+          if (this.isCoordinator) {
+            this.getCoordinatorInstitution();
+          }
       }
     });
 
@@ -46,6 +56,9 @@ export class NavbarComponent {
     this.user = this.accountService.getUserData();
     this.isAdmin = this.accountService.isAdmin();
     this.isCoordinator = this.accountService.isCoordinator();
+    if (this.isCoordinator) {
+      this.getCoordinatorInstitution();
+    }
   }
 
   refreshIsLoggedIn(): void {
@@ -64,5 +77,29 @@ export class NavbarComponent {
     this.tokenService.removeToken();
     this.isLoggedIn = false;
     this.router.navigate(['/login']);
+  }
+
+  getCoordinatorInstitution(): void {
+    const userId = this.accountService.getUserId();
+    if (userId) {
+      this.applicationService.getHomeInstitutionIdByUserId(userId).subscribe({
+        next: (result) => {
+          if (result.success) {
+            const homeInstitutionId = result.data;
+            this.homeInstitutionsService.getAll().subscribe({
+              next: (institutionsResult) => {
+                if (institutionsResult.success) {
+                  const institution = institutionsResult.data.find((inst: any) => inst.id === homeInstitutionId);
+                  if (institution) {
+                    this.coordinatorInstitutionName = institution.name;
+                    // console.log(this.coordinatorInstitutionName);
+                  }
+                }
+              }
+            });
+          }
+        }
+      });
+    }
   }
 }
